5a04477f2144ff50693e1801ca2d733035599c96
Devconte <romain.lcnte@gmail.com>
2024-04-17 15:35:35 +0200

    feat: added drawing crds, displaying images and remaining card and disabling when no more cards

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 let deckId
 
+const newDeckBtn = document.getElementById("new-deck")
+
+const drawCardBtn = document.getElementById("draw-cards")
+
+const cardsContainer = document.getElementById("cards-container")
+
+const winnerText = document.getElementById("winner-text")
+const remainingText = document.getElementById("remaining-cards")
+
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            
             deckId = data.deck_id
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
         })
 }
 
-if (!deckId){
-    document.getElementById('draw-cards').setAttribute('disabled', true)
-}
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 /**
  * Challenge
  * 
@@ -24,13 +31,60 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
  * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
  * that you'll see in the deck of cards API docs.
  */
+function handleWin(card1, card2){
+    if(card1.value > card2.value){
+        return "Computer wins"
+    } else if(card1.value < card2.value){
+        return "Player wins"
+    } else {
+        return "It's a tie"
+    }
+}
 
-document.getElementById('draw-cards').addEventListener('click', () => {
+
+drawCardBtn.addEventListener('click', () => {
     
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        remainingText.textContent = `Remaining cards: ${data.remaining} cards left`
+        cardsContainer.children[0].innerHTML = `
+        <img src=${data.cards[0].image} class="card" />
+        `
+        cardsContainer.children[1].innerHTML = `
+        <img src=${data.cards[1].image} class="card" />`
+        winnerText.textContent = handleWin(data.cards[0], data.cards[1])
+        if(data.remaining === 0){
+            handleNoCardsLeft()
+        }
+  
     })
 });
+
+function handleNoCardsLeft(){
+    drawCardBtn.setAttribute("disabled", true);
+    drawCardBtn.setAttribute("style", "background-color: grey");
+    drawCardBtn.setAttribute("style" , "cursor: not-allowed");
+    remainingText.textContent = "No cards left in the deck";
+    
+}
+
+
+
+// function determineCardWinner(card1, card2) {
+//     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+//     "10", "JACK", "QUEEN", "KING", "ACE"]
+//     const card1ValueIndex = valueOptions.indexOf(card1.value)
+//     const card2ValueIndex = valueOptions.indexOf(card2.value)
+//     console.log("card 1:", card1ValueIndex)
+//     console.log("card 2:", card2ValueIndex)
+    
+//     if (card1ValueIndex > card2ValueIndex) {
+//         console.log("Card 1 wins!")
+//     } else if (card1ValueIndex < card2ValueIndex) {
+//         console.log("Card 2 wins!")
+//     } else {
+//         console.log("It's a tie!")
+//     }
+// }
 
